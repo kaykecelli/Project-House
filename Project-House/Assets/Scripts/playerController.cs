9bcdeb526c6294e778df3e67d0447b8a91990c4f
kaykecelli <Kaykecelli6@gmail.com>
2022-10-13 07:22:00 -0300

    HouseCodeV1.0

---
+++ Project-House/Assets/Scripts/playerController.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.5f;
+    
+
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementIput;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+  
+    
+    private void FixedUpdate()
+    {
+        if (movementIput != Vector2.zero)
+        {
+            bool success = TryMove(movementIput);
+           
+
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementIput.x, 0));
+            }
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(0, movementIput.y));
+            }
+            animator.SetFloat("VELOCIDADE", 1);
+            
+        }
+        else
+        {
+            animator.SetFloat("VELOCIDADE", -1);
+            
+        }
+
+        
+    }
+   private bool TryMove(Vector2 direction)
+    {
+        Vector2 direcao = new Vector2(direction.x, direction.y);
+      
+        animator.SetFloat("HORIZONTAL", direction.x);
+        animator.SetFloat("VERTICAL", direction.y);
+
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(
+                   direcao,
+                   movementFilter,
+                   castCollision,
+                   moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direcao * moveSpeed * Time.fixedDeltaTime);
+                return true;
+
+            }
+            
+            else
+            {
+                return false;
+            }
+
+        }
+        else
+        {
+            return false;
+        }
+       
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    void OnMove(InputValue movementValue)
+    {
+        movementIput = movementValue.Get<Vector2>();
+
+
+    }
+}
